7f1e06fd9f83c441fe2b817fd9afaadbc4196d17
Lexi127 <aperalta127@gmail.com>
2019-05-24 13:17:43 -0700

    created database, tables, && verified function

---
+++ models/fitness.js
@@ -0,0 +1,20 @@
+module.exports = function(sequelize, DataTypes) {
+  var Fitness = sequelize.define("Fitness", {
+    cal_goals: DataTypes.INTEGER,
+    cal_burned: DataTypes.INTEGER,
+    cal_intake: DataTypes.INTEGER,
+    user_height: DataTypes.INTEGER,
+    user_weight: DataTypes.INTEGER,
+    user_age: DataTypes.INTEGER,
+  });
+
+  Fitness.associate = function(models) {
+    Fitness.belongsTo (models.User, {
+        foreignKey: {
+        allowNull: false
+      }
+    })
+  }
+  return Fitness;
+};
+
