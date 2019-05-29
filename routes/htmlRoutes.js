97673b986c50633a039f87e70e42dc28d15c3d4b
tyler stuart <t.stu21.11@gmail.com>
2019-05-29 13:55:07 -0700

    working calculators

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,13 +9,23 @@ module.exports = function (app) {
   
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/calcCal", function (req, res) {
+    res.render("calcCal");
+   
     });
-  });
+  
+
+    app.get("/graph", function (req, res) {
+      res.render("graph");
+     
+      });
+
+      app.get("/manburnt", function (req, res) {
+        res.render("manburnt");
+       
+        });
+      
+    
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
