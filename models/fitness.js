4e586f6a689d9acff069a522ba7c1b4df158e15e
tyler stuart <t.stu21.11@gmail.com>
2019-05-29 12:13:53 -0700

    sequelize

--- models/fitness.js
+++ models/fitness.js
@@ -1,13 +1,12 @@
 module.exports = function (sequelize, DataTypes) {
   var Fitness = sequelize.define("Fitness", {
-    cal_goals: DataTypes.INTEGER,
-    cal_burned: DataTypes.INTEGER,
-    cal_intake: DataTypes.INTEGER,
-    user_height: DataTypes.INTEGER,
-    user_weight: DataTypes.INTEGER,
-    user_age: DataTypes.INTEGER,
+    cal_goals: Sequelize.INTEGER,
+    cal_burned: Sequelize.INTEGER,
+    cal_intake: Sequelize.INTEGER,
+    user_height: Sequelize.INTEGER,
+    user_weight: Sequelize.INTEGER,
+    user_age: Sequelize.INTEGER,
   });
-
   Fitness.associate = function (models) {
     Fitness.belongsTo(models.User, {
       foreignKey: {
