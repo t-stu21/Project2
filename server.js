1477892dde815581af3778d5e364f4f1d1ac06b6
Lexi127 <aperalta127@gmail.com>
2019-05-31 21:15:11 -0700

    db updates

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('*', function (req, res) {
   res.render('404');
 });
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
