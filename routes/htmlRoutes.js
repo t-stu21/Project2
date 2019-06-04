7387cc3a18734b75e4d765518e54d192c2d2debe
Mourizio Zeland <zelandmourizio@gmail.com>
2019-06-03 18:33:40 -0700

    LoginPage

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,6 +30,10 @@ module.exports = function (app) {
           res.render("dietary");
          
           });  
+        app.get("/login", function (req, res) {
+            res.render("login");
+           
+            });  
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
