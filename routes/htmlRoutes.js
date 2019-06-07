93657db3e284ff3c982e75df280b4e049a78b987
tyler stuart <t.stu21.11@gmail.com>
2019-06-07 11:55:02 -0700

    fixes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,37 +1,37 @@
-var path = require('path');
-var authController = require('../controllers/authcontroller.js');
-var db = require('../models');
+var path = require("path");
+var authController = require("../controllers/authcontroller.js");
+var db = require("../models");
 
-var schedule = require('./schedule');
+var schedule = require("./schedule");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get('/', function (req, res) {
-    res.render('index', { schedule });
+  app.get("/", function(req, res) {
+    res.render("index", { schedule });
   });
 
-  app.get('/manburnt', function (req, res) {
-    res.render('manburnt', { schedule });
+  app.get("/manburnt", function(req, res) {
+    res.render("manburnt", { schedule });
   });
 
-  app.get('/userinfo', function (req, res) {
-    res.render('userinfo');
+  app.get("/userinfo", function(req, res) {
+    res.render("userinfo");
   });
 
-  app.get('/exercise', function (req, res) {
-    res.render('exercise', { schedule });
+  app.get("/exercise", function(req, res) {
+    res.render("exercise", { schedule });
   });
 
-  app.get('/addworkout', function (req, res) {
-    res.render('addworkoutday');
+  app.get("/addworkout", function(req, res) {
+    res.render("addworkoutday");
   });
 
-  app.get('/workoutday', function (req, res) {
-    res.render('workoutday');
+  app.get("/workoutday", function(req, res) {
+    res.render("workoutday");
   });
 
-  app.get('/users', function (req, res) {
-    res.render('user-manager');
+  app.get("/users", function(req, res) {
+    res.render("user-manager");
   });
 
   app.get("/dailygoals", function(req, res) {
