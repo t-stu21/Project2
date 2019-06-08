2e6c9471a4a77a1d296605b7164fb099404cb9fe
Mourizio Zeland <zelandmourizio@gmail.com>
2019-06-08 00:06:44 -0700

    Calendar

--- public/js/dietary.js
+++ public/js/dietary.js
@@ -1,8 +1,43 @@
-fatsecret.setContainer("my_container");
-fatsecret.setCanvas("home");
- 
+document.addEventListener('DOMContentLoaded', function() {
+  var Calendar = FullCalendar.Calendar;
+  var Draggable = FullCalendarInteraction.Draggable;
 
+  var containerEl = document.getElementById('external-events');
+  var calendarEl = document.getElementById('calendar');
+  var checkbox = document.getElementById('drop-remove');
 
+  // initialize the external events
+  // -----------------------------------------------------------------
 
+  new Draggable(containerEl, {
+    itemSelector: '.fc-event',
+    eventData: function(eventEl) {
+      return {
+        title: eventEl.innerText
+      };
+    }
+  });
 
+  // initialize the calendar
+  // -----------------------------------------------------------------
 
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    editable: true,
+    droppable: true, // this allows things to be dropped onto the calendar
+    drop: function(info) {
+      // is the "remove after drop" checkbox checked?
+      if (checkbox.checked) {
+        // if so, remove the element from the "Draggable Events" list
+        info.draggedEl.parentNode.removeChild(info.draggedEl);
+      }
+    }
+  });
+
+  calendar.render();
+});
