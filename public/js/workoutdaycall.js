f815af1fb887d95286528e141a4d1966ebd4ab37
Kevin Flanagan <kflan3000@gmail.com>
2019-06-07 21:07:45 -0700

    logged in user id obtained for url

--- public/js/workoutdaycall.js
+++ public/js/workoutdaycall.js
@@ -78,6 +78,7 @@ $(document).ready(function() {
     if (url.indexOf('?user_id=') !== -1) {
       let uid = url.split('=')[1];
       userID = parseInt(uid);
+      USERID = userID;
     }
     return userID;
   };
