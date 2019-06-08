fb2d1cf42c987b875db713893b944d98efb1d9fc
tyler stuart <t.stu21.11@gmail.com>
2019-06-08 08:51:15 -0700

    heroku fix

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var passport = require('passport');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
 // var env = require('dotenv').load();
-var exphbs = require('express-handlebars');
-var user = require('./models/user.js');
-var pport = require('./config/passport/passport.js');
-
+var exphbs = require("express-handlebars");
+var user = require("./models/user.js");
+var pport = require("./config/passport/passport.js");
 
 //models
-var models = require('./models');
+var models = require("./models");
 
 //load passport strategies
-require('./config/passport/passport.js')(passport, models.user);
+require("./config/passport/passport.js")(passport, models.user);
 
-var db = require('./models');
+var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.port || 3000;
 
 //For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +26,7 @@ app.use(bodyParser.json());
 //session secret
 app.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: true,
     saveUninitialized: true
   })
@@ -39,49 +38,52 @@ app.use(passport.session());
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Handlebars
-app.set('views', './views');
+app.set("views", "./views");
 app.engine(
-  'handlebars',
+  "handlebars",
   exphbs({
-    defaultLayout: 'main'
+    defaultLayout: "main"
   })
 );
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 // Routes
-require('./routes/user-api-routes')(app);
-require('./routes/workoutday-api-routes')(app);
-require('./routes/htmlRoutes')(app);
+require("./routes/user-api-routes")(app);
+require("./routes/workoutday-api-routes")(app);
+require("./routes/htmlRoutes")(app);
 //auth routes for passport
-require('./routes/auth.js')(app, passport);
+require("./routes/auth.js")(app, passport);
 // Render 404 page for any unmatched routes
-app.get('*', function (req, res) {
-  res.render('404');
+app.get("*", function(req, res) {
+  res.render("404");
 });
 
 var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-
 // Starting the server, syncing our models ------------------------------------/
-models.sequelize.sync().then(function () {
-  console.log('Nice! Database looks fine');
+models.sequelize.sync().then(function() {
+  console.log("Nice! Database looks fine");
 
-  app.listen(PORT, function () {
-    console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+  app.listen(port, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      port,
+      port
+    );
   });
 
   // Passport
-  app.get('/', function (req, res) {
-    res.send('Welcome to Passport with Sequelize');
+  app.get("/", function(req, res) {
+    res.send("Welcome to Passport with Sequelize");
   });
 });
 
